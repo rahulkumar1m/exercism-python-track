bc6a4caa9d37768c9cd3832e11850510d14efad5
Rahul <rahulkumar10m@outlook.com>
2021-01-13 10:35:07 +0530

    Solution to protein_translation problem

--- protein-translation/protein_translation.py
+++ protein-translation/protein_translation.py
@@ -1,2 +1,30 @@
+codon_mapping = {'AUG': 'Methionine', 
+    'UUU': 'Phenylalanine', 
+    'UUC': 'Phenylalanine', 
+    'UUA': 'Leucine', 
+    'UUG': 'Leucine', 
+    'UCU': 'Serine', 
+    'UCC': 'Serine', 
+    'UCA': 'Serine', 
+    'UCG': 'Serine', 
+    'UAU': 'Tyrosine', 
+    'UAC': 'Tyrosine', 
+    'UGU': 'Cysteine', 
+    'UGC': 'Cysteine', 
+    'UGG': 'Tryptophan', 
+    'UAA': 'STOP', 
+    'UAG': 'STOP', 
+    'UGA': 'STOP'
+    }
+
 def proteins(strand):
-    pass
+    codons = (strand[i:i+3] for i in range(0, len(strand), 3))
+    protein_translation = []
+
+    for i in list(codons):
+        protein = codon_mapping[i]
+        if protein == 'STOP':
+            break
+        protein_translation.append(protein)
+    
+    return protein_translation
