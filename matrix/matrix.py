099af37b2f787f53f9d813a5f3d095d2328f5245
Rahul <rahulkumar10m@outlook.com>
2021-01-18 13:14:44 +0530

    Solution - matrix problem

--- matrix/matrix.py
+++ matrix/matrix.py
@@ -1,9 +1,21 @@
 class Matrix:
-    def __init__(self, matrix_string):
-        pass
+    def __init__(self, matrix_string: str):
+        self.matrix = []
+        rows = matrix_string.split('\n')
+        
+        for row in rows:
+            row = row.split(' ')
+            row = list(map(int, row))
+            self.matrix.append(row)
+        print(self.matrix)
 
-    def row(self, index):
-        pass
 
-    def column(self, index):
-        pass
+    def row(self, index: int) -> list:
+        return self.matrix[index-1]
+
+
+    def column(self, index: int) -> list:
+        column = []
+        for row in self.matrix:
+            column.append(row[index-1])
+        return column
