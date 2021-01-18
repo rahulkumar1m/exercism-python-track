acd42d4719385e4f796f96f1cbc9730630d7f42d
Rahul <rahulkumar10m@outlook.com>
2021-01-18 12:54:30 +0530

    Solution to perfect number

--- perfect-numbers/perfect_numbers.py
+++ perfect-numbers/perfect_numbers.py
@@ -1,2 +1,12 @@
 def classify(number):
-    pass
+    if number < 1:
+        raise ValueError("Classification is not possible")
+    
+    factors = [a for a in range(1, int(number/2)+1) if number % a == 0]
+
+    if sum(factors) == number:
+        return "perfect"
+    elif sum(factors) > number:
+        return "abundant"
+    else:
+        return "deficient"
