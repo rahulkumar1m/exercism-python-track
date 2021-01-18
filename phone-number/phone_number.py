59ce77e78abad2d9dd4ef7d474362a15d763a702
Rahul <rahulkumar10m@outlook.com>
2021-01-18 21:34:20 +0530

    Solution of Phone-number problem

--- phone-number/phone_number.py
+++ phone-number/phone_number.py
@@ -1,3 +1,14 @@
+import re
+
 class PhoneNumber:
-    def __init__(self, number):
-        pass
+    def __init__(self, number: str):
+        self.pattern = re.compile(r'^\+?1?(?:[-. ]+)?\(?([2-9]\d{2})\)?(?:[-. ]+)?([2-9]\d{2})(?:[-. ]+)?(\d{4})$')
+        try:
+            self.area_code, self.exchange, self.subscriber = \
+                self.pattern.search(number.strip()).groups()
+        except:
+            raise ValueError('Invalid `phone number`')
+        self.number = self.area_code + self.exchange + self.subscriber
+    
+    def pretty(self)-> str:
+        return f'({self.area_code})-{self.exchange}-{self.subscriber}'
