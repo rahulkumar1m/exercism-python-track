111700ce6b35cb96fda635b8fa0b443d08765729
Rahul <rahulkumar10m@outlook.com>
2021-01-18 13:48:49 +0530

    Solution to Series Problem

--- series/series.py
+++ series/series.py
@@ -1,2 +1,16 @@
-def slices(series, length):
-    pass
+def slices(series: str, length: int):
+    if len(series) == 0:
+        raise ValueError("Empty Series")
+    if length < 0:
+        raise ValueError("Length is negative")
+    if length == 0:
+        raise ValueError("Length is Zero")
+    if len(series) < length:
+        raise ValueError("Length is too large")
+    slice = []
+    for i in range(0, len(series) - length + 1):
+        s = ''
+        for j in range(i, i+length):
+            s += series[j]
+        slice.append(s)
+    return slice
