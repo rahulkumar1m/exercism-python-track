3e2af05861c336b3e7dbbb0b6162c1350e38f5d2
Rahul <rahulkumar10m@outlook.com>
2021-01-20 13:03:15 +0530

    Solution - SpaceAge & isbn verifier

--- isbn-verifier/isbn_verifier.py
+++ isbn-verifier/isbn_verifier.py
@@ -1,2 +1,13 @@
-def is_valid(isbn):
-    pass
+import re
+
+
+def is_valid(isbn: str) -> bool:
+    isbn_ = re.compile('^([-0-9]+)(X)?$')
+    check_match = isbn_.match(isbn)
+    if (check_match):
+        valid_chars = [int(n) for n in check_match.group(1) if n != '-']
+        if check_match.group(2):
+            valid_chars.append(10)
+        if len(valid_chars) == 10:
+            return sum([num * (10-index) for index, num in enumerate(valid_chars)]) % 11 == 0
+    return False
--- space-age/space_age.py
+++ space-age/space_age.py
@@ -1,3 +1,27 @@
 class SpaceAge:
     def __init__(self, seconds):
-        pass
+        self.earth_age = (1.0 * seconds)/(31557600)
+    
+    def on_earth(self):
+        return round(self.earth_age, 2)
+    
+    def on_mercury(self) -> float:
+        return round(self.earth_age/0.2408467, 2)
+    
+    def on_venus(self) -> float:
+        return round(self.earth_age/0.61519726, 2)
+    
+    def on_mars(self) -> float:
+        return round(self.earth_age/1.8808158, 2)
+    
+    def on_jupiter(self) -> float:
+        return round(self.earth_age/11.862615, 2)
+    
+    def on_saturn(self) -> float:
+        return round(self.earth_age/29.447498, 2)
+    
+    def on_uranus(self) -> float:
+        return round(self.earth_age/84.016846, 2)
+    
+    def on_neptune(self) -> float:
+        return round(self.earth_age/164.79132, 2)
