4b7f1ddafd05db9674c6f59ad8c10fdc712c6979
Rahul <rahulkumar10m@outlook.com>
2021-01-20 13:53:38 +0530

    Solution to rational Numbers

--- rational-numbers/rational_numbers.py
+++ rational-numbers/rational_numbers.py
@@ -1,10 +1,11 @@
 from __future__ import division
-
+from fractions import gcd
 
 class Rational:
     def __init__(self, numer, denom):
-        self.numer = None
-        self.denom = None
+        _gcd = gcd(numer, denom)
+        self.numer = numer / _gcd
+        self.denom = denom / _gcd
 
     def __eq__(self, other):
         return self.numer == other.numer and self.denom == other.denom
@@ -13,22 +14,48 @@ class Rational:
         return '{}/{}'.format(self.numer, self.denom)
 
     def __add__(self, other):
-        pass
+        return Rational(
+            self.numer * other.denom + other.numer * self.denom,
+            self.denom * other.denom
+        )
 
     def __sub__(self, other):
-        pass
+        return Rational(
+            self.numer * other.denom - other.numer * self.denom,
+            self.denom * other.denom
+        ) 
 
     def __mul__(self, other):
-        pass
+        return Rational(
+            self.numer * other.numer,
+            self.denom * other.denom
+        )
 
     def __truediv__(self, other):
-        pass
+        return Rational(
+            self.numer * other.denom,
+            self.denom * other.numer
+        )
 
     def __abs__(self):
-        pass
+        return Rational(
+            abs(self.numer),
+            abs(self.denom)
+        )
 
     def __pow__(self, power):
-        pass
+        if power > 0:
+            return Rational(
+                self.numer ** power,
+                self.denom ** power
+            )
+        elif power == 0:
+            return Rational(1, 1)
+        else:
+            return Rational(
+                self.denom ** abs(power),
+                self.numer ** abd(power)
+            )
 
     def __rpow__(self, base):
-        pass
+        return (base ** self.numer) ** (1 / self.denom)
