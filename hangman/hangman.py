fa752e9efacbedee98d25e38c4c00a0b0c2d712d
Rahul <rahulkumar10m@outlook.com>
2021-01-21 16:41:12 +0530

    Solution - hangman

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -4,17 +4,36 @@ STATUS_WIN = "win"
 STATUS_LOSE = "lose"
 STATUS_ONGOING = "ongoing"
 
-
 class Hangman:
     def __init__(self, word):
         self.remaining_guesses = 9
         self.status = STATUS_ONGOING
+        self.word = word
+        self.characters = ''
+        for char in self.word:
+            self.characters = self.characters + '_'
 
     def guess(self, char):
-        pass
+        if self.status == STATUS_WIN or self.status == STATUS_LOSE:
+            raise ValueError("Invalid Guess")
+        elif char in self.word and char not in self.characters:
+            cont = 0
+            for c in self.word:
+                if c == char:
+                    self.characters = list(self.characters)
+                    self.characters[cont] = char
+                    self.characters == "".join(self.characters)
+                cont += 1
+        else:
+            self.remaining_guesses -= 1            
 
     def get_masked_word(self):
-        pass
+        return "".join(self.characters)
 
     def get_status(self):
-        pass
+        if '_' not in self.characters:
+            self.status = STATUS_WIN
+        elif self.remaining_guesses <= 0:
+            self.status = STATUS_LOSE
+
+        return self.status
