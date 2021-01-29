1c3df24aac3dc87804a3687c8406f8bacbaf6de3
Rahul <rahulkumar10m@outlook.com>
2021-01-29 16:33:06 +0530

    Optimized solution - Matrix

--- matrix/matrix.py
+++ matrix/matrix.py
@@ -1,13 +1,13 @@
 class Matrix:
     def __init__(self, matrix_string: str):
-        self.matrix = []
-        rows = matrix_string.split('\n')
-        
-        for row in rows:
-            row = row.split(' ')
-            row = list(map(int, row))
-            self.matrix.append(row)
-        print(self.matrix)
+        rows = matrix_string.splitlines()
+        self.matrix = [list(map(int, row.split())) for row in rows]
+
+        # for row in rows:
+        #     row = row.split(' ')
+        #     row = list(map(int, row))
+        #     self.matrix.append(row)
+        # print(self.matrix)
 
 
     def row(self, index: int) -> list:
@@ -15,7 +15,8 @@ class Matrix:
 
 
     def column(self, index: int) -> list:
-        column = []
-        for row in self.matrix:
-            column.append(row[index-1])
+        column = [row[index-1] for row in self.matrix]
+        # for row in self.matrix:
+        #     column.append(row[index-1])
         return column
+
