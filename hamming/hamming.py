b87fb20a2ac4a0979a6bfb53176055fdb73b31d6
Rahul <rahulkumar10m@outlook.com>
2021-01-31 13:20:50 +0530

    Hamming Solution

--- hamming/hamming.py
+++ hamming/hamming.py
@@ -1,2 +1,14 @@
-def distance(strand_a, strand_b):
-    pass
+def distance(strand_a : str, strand_b : str) -> int:
+    # Raise ValueErrors if length of passed DNA sequences do not match
+    if len(strand_a) != len(strand_b):
+        raise ValueError("Length of DNA sequences DO NOT MATCH")
+    
+    # Initializing hammin_distance with 0
+    hamming_distance = 0
+    
+    # Checking for differences between the two sequences
+    for i in range(len(strand_a)):
+        if strand_a[i] != strand_b[i]:
+            hamming_distance += 1
+    
+    return hamming_distance
