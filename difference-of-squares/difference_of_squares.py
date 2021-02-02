3ac4f63dc0d8d082505916265facb49ab6e30042
Rahul <rahulkumar10m@outlook.com>
2021-02-02 11:24:24 +0530

    Added return and parameter types

--- difference-of-squares/difference_of_squares.py
+++ difference-of-squares/difference_of_squares.py
@@ -1,10 +1,10 @@
-def square_of_sum(number):
+def square_of_sum(number: int) -> int:
     return (((number+1)*number)/2)**2
 
 
-def sum_of_squares(number):
+def sum_of_squares(number: int) -> int:
     return sum([x**2 for x in range(number+1)])
 
 
-def difference_of_squares(number):
+def difference_of_squares(number: int) -> int:
     return square_of_sum(number) - sum_of_squares(number)
