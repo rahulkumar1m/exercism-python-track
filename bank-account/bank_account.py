f8633bc85b34d21d33beb4da644117dc48006e88
Rahul <rahulkumar10m@outlook.com>
2021-02-02 11:15:43 +0530

    Solution Bank-Account problem

--- bank-account/bank_account.py
+++ bank-account/bank_account.py
@@ -1,18 +1,35 @@
 class BankAccount:
     def __init__(self):
-        pass
+        self.status = 'CLOSE'
+        self.balance = 0
 
     def get_balance(self):
-        pass
+        if self.status == 'OPEN':
+            return self.balance
+        else:
+            raise ValueError('Closed Account')
 
     def open(self):
-        pass
+        if self.status == 'CLOSE':
+            self.status = 'OPEN'
+        else:
+            raise ValueError('Already Opened Account')
 
     def deposit(self, amount):
-        pass
+        if (self.status == 'OPEN') & (amount >= 0):
+            self.balance += amount
+        else:
+            raise ValueError('Account Closed')
 
     def withdraw(self, amount):
-        pass
+        if (self.status == 'OPEN') & (self.balance >= amount) & (amount >= 0):
+            self.balance -= amount
+        else:
+            raise ValueError('Account Closed / Withdraw amount > balance amount / Cannot withdraw negative amount')
 
     def close(self):
-        pass
+        if self.status == 'OPEN':
+            self.status = 'CLOSE'
+            self.balance = 0
+        else:
+            raise ValueError('Account already closed')
