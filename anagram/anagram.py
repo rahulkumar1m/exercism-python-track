a6b4a1a0405707d55bf173baa0bfbbb542667501
Rahul <rahulkumar10m@outlook.com>
2021-02-03 11:16:11 +0530

    Anagram solution

--- anagram/anagram.py
+++ anagram/anagram.py
@@ -1,2 +1,34 @@
 def find_anagrams(word, candidates):
-    pass
+    dict_word = {}
+    expected_list = []
+
+    for char in word.lower():
+        if char in dict_word:
+            dict_word[char] += 1
+        else:
+            dict_word[char] = 1
+    
+    for candidate in candidates:
+        if (len(candidate) != len(word)) | (candidate.lower() == word.lower()):
+            continue
+
+        dict_candidate = {}
+        for char in candidate.lower():
+            if char in dict_candidate:
+                dict_candidate[char] += 1
+            else:
+                dict_candidate[char] = 1
+        
+        anagram = True
+        for key, value in dict_candidate.items():
+            if key not in dict_word:
+                anagram = False
+                break
+            elif value != dict_word[key]:
+                anagram = False
+                break
+        
+        if anagram:
+            expected_list.append(candidate)
+    
+    return expected_list
