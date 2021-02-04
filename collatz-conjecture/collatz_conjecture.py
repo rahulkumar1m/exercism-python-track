94faeb0e6fb93227d0f852f2bb8c1102b231c75a
Rahul <rahulkumar10m@outlook.com>
2021-02-04 11:08:22 +0530

    Solution collatz-conjecture

--- collatz-conjecture/collatz_conjecture.py
+++ collatz-conjecture/collatz_conjecture.py
@@ -1,2 +1,10 @@
-def steps(number):
-    pass
+def steps(number: int) -> int:
+    if number <= 0:
+        raise ValueError("Negative Number is not allowed")
+
+    if number == 1:
+        return 0
+    elif number % 2 == 0:
+        return steps(number/2) + 1
+    else:
+        return steps(3 * number + 1) + 1
