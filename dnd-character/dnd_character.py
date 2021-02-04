8dd58001fd51610a92cfd8a7d77a697a8c44066b
Rahul <rahulkumar10m@outlook.com>
2021-02-04 10:51:52 +0530

    Added problems & solution to dnd-character

---
+++ dnd-character/dnd_character.py
@@ -0,0 +1,23 @@
+import random
+
+abilities = (
+    'strength',
+    'dexterity',
+    'constitution',
+    'intelligence',
+    'wisdom',
+    'charisma'
+)
+
+def modifier(score: int) -> int:
+    return (score - 10) // 2
+
+class Character:
+    def __init__(self):
+        for ability in abilities:
+            setattr(self, ability, self.ability())
+        self.hitpoints = 10 + modifier(self.constitution)
+    
+    def ability(self):
+        dices = sorted(random.randint(1,6) for i in range(4))
+        return sum(dices[1:])
