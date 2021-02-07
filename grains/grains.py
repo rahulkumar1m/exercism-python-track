e1b836955278b4426e00de8084a49695ec02ef99
Rahul <rahulkumar10m@outlook.com>
2021-02-07 14:12:20 +0530

    Solutio Grains

--- grains/grains.py
+++ grains/grains.py
@@ -1,6 +1,9 @@
 def square(number):
-    pass
+    if (number > 0) & (number <= 64):
+        return 2**(number-1)
+    else:
+        raise ValueError("Negative Numbers or Numbers > 64 not allowed")
 
 
 def total():
-    pass
+    return int(sum([2**(number) for number in range(64)]))
