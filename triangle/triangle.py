b443017933df9064dc2c1fb512d26a01e955e5ec
Rahul <rahulkumar10m@outlook.com>
2021-02-07 16:25:12 +0530

    Solution triangle

--- triangle/triangle.py
+++ triangle/triangle.py
@@ -1,10 +1,19 @@
 def equilateral(sides):
-    pass
+    if (min(sides) <= 0) | (sides[0] + sides[1] <= sides[2]) | (sides[1] + sides[2] <= sides[0]) | (sides[2] + sides[0] <= sides[1]):
+        return False
+    else:
+        return (sides[0] == sides[1]) & (sides[1] == sides[2])
 
 
 def isosceles(sides):
-    pass
+    if (min(sides) <= 0) | (sides[0] + sides[1] <= sides[2]) | (sides[1] + sides[2] <= sides[0]) | (sides[2] + sides[0] <= sides[1]):
+        return False
+    else:
+        return (sides[0] == sides[1]) | (sides[1] == sides[2]) | (sides[2] == sides[0])
 
 
 def scalene(sides):
-    pass
+    if (min(sides) <= 0) | (sides[0] + sides[1] <= sides[2]) | (sides[1] + sides[2] <= sides[0]) | (sides[2] + sides[0] <= sides[1]):
+        return False
+    else:
+        return (sides[0] != sides[1]) & (sides[1] != sides[2]) & (sides[2] != sides[0])
