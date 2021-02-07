291363f6e60836712203e901f24d800d06163030
Rahul <rahulkumar10m@outlook.com>
2021-02-07 16:10:57 +0530

    Solution palindrome-products

--- palindrome-products/palindrome_products.py
+++ palindrome-products/palindrome_products.py
@@ -1,6 +1,24 @@
+def palindrome(min_factor, max_factor, largest):
+    if min_factor > max_factor:
+        raise ValueError(ValueError)
+
+    args = (max_factor ** 2, min_factor**2 + 1, -1) if largest else (min_factor**2, max_factor**2+1)
+    palin, factors = None, []
+    for num in range(*args):
+        if str(num) == str(num)[::-1]:
+            valid = False
+            for i in range(min_factor, max_factor + 1):
+                if num % i == 0 and i <= (num // i) <= max_factor:
+                    factors.append([i, num // i])
+                    palin, valid = num, True
+            if valid:
+                break
+
+    return palin, factors
+
 def largest(min_factor, max_factor):
-    pass
+    return palindrome(min_factor, max_factor, largest=True)
 
 
 def smallest(min_factor, max_factor):
-    pass
+    return palindrome(min_factor, max_factor, largest=False)
