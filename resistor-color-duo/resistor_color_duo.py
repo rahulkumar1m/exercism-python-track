ad1b088bd0fbfc901a0dc495a16b57f99514f17a
Rahul <rahulkumar10m@outlook.com>
2021-02-07 14:05:51 +0530

    Solution resistor_color_duo problem

--- resistor-color-duo/resistor_color_duo.py
+++ resistor-color-duo/resistor_color_duo.py
@@ -1,2 +1,15 @@
-def value(colors):
-    pass
+def value(colors) -> int:
+    color_code = {
+        "black" : 0,
+        "brown" : 1,
+        "red" : 2,
+        "orange" : 3,
+        "yellow" : 4,
+        "green" : 5,
+        "blue" : 6,
+        "violet" : 7,
+        "grey" : 8,
+        "white" : 9
+    }
+
+    return color_code[colors[0]]*10 + color_code[colors[1]]
