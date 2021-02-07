cb793f97dc0a57c6ecef38e7af70fbe5a424506d
Rahul <rahulkumar10m@outlook.com>
2021-02-07 13:51:57 +0530

    Solution -darts.py

--- darts/darts.py
+++ darts/darts.py
@@ -1,2 +1,13 @@
-def score(x, y):
-    pass
+import math
+
+def score(x : int, y : int) -> int:
+    circle = math.sqrt(x**2 + y**2)
+
+    if circle <= 1:
+        return 10
+    elif circle <= 5:
+        return 5
+    elif circle <= 10:
+        return 1
+    else:
+        return 0
