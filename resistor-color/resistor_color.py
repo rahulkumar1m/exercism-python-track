c220ed8c0616c17564c467c84320dbfd3daa42dc
Rahul <rahulkumar10m@outlook.com>
2021-02-07 13:57:35 +0530

    Solution resistor-color.py

--- resistor-color/resistor_color.py
+++ resistor-color/resistor_color.py
@@ -1,6 +1,29 @@
 def color_code(color):
-    pass
+    color_code = {
+        "black" : 0,
+        "brown" : 1,
+        "red" : 2,
+        "orange" : 3,
+        "yellow" : 4,
+        "green" : 5,
+        "blue" : 6,
+        "violet" : 7,
+        "grey" : 8,
+        "white" : 9
+    }
+    return color_code[color]
 
 
 def colors():
-    pass
+    return [
+            "black",
+            "brown",
+            "red",
+            "orange",
+            "yellow",
+            "green",
+            "blue",
+            "violet",
+            "grey",
+            "white",
+        ]
