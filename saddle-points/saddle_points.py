8a8145c26a5e86316fd70d703587234988c1383c
Rahul <rahulkumar10m@outlook.com>
2021-02-07 15:16:18 +0530

    Saddle Points solution

--- saddle-points/saddle_points.py
+++ saddle-points/saddle_points.py
@@ -1,2 +1,12 @@
 def saddle_points(matrix):
-    pass
+    if len(set(map(len, matrix))) > 1:
+        raise ValueError("Irregular Matrix")
+
+    matrix_ = list(zip(*matrix))
+    points = []
+
+    for i, row in enumerate(matrix):
+        for j, x in enumerate(row):
+            if x == max(row) and x == min(matrix_[j]):
+                points.append({"row" : i+1, "column" : j+1})
+    return points
