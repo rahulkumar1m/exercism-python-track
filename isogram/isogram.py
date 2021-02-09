1e1a198b38b6e5bc880c18a5b4fff47c828ac113
Rahul <rahulkumar10m@outlook.com>
2021-02-09 23:06:48 +0530

    Solution to isogram

--- isogram/isogram.py
+++ isogram/isogram.py
@@ -1,2 +1,17 @@
-def is_isogram(string):
-    pass
+def is_isogram(string) -> bool:
+    # Tokenizing the characters in the string
+    string = [char for char in string.lower()]
+
+    # initializing an empty list of characters present in the string
+    characters = []
+
+    # if a character from string is already present in our list of characters, we return False
+    for char in string:
+        if (char == " ") or (char == "-"):
+            continue
+        elif char in characters:
+            return False
+        else:
+            characters.append(char)
+    
+    return True
