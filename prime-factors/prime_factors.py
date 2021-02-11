fec1b423407e52b2e9462bd4612f61b80c6fa340
Rahul <rahulkumar10m@outlook.com>
2021-02-11 18:46:15 +0530

    Solution to Prime-factors problem

--- prime-factors/prime_factors.py
+++ prime-factors/prime_factors.py
@@ -1,2 +1,17 @@
+import math
+
 def factors(value):
-    pass
+    prime_factors = []
+    while value % 2 == 0:
+        prime_factors.append(2)
+        value /= 2
+    
+    for i in range(3, int(math.sqrt(value)) + 1):
+        while value % i == 0:
+            prime_factors.append(i)
+            value /= i
+    
+    if value > 2:
+        prime_factors.append(value)
+
+    return prime_factors
