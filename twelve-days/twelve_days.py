84ab2d8e7b0b9e5d551b1691291d3f0dd1f35437
Rahul <32988067+rahulkumar1m@users.noreply.github.com>
2021-03-16 10:37:43 +0530

    Twelve days solution

--- twelve-days/twelve_days.py
+++ twelve-days/twelve_days.py
@@ -1,2 +1,17 @@
-def recite(start_verse, end_verse):
-    pass
+def recite(start_verse : int, end_verse : int):
+    days = ['first', 'second', 'third', 'fourth', 'fifth', 'sixth', 'seventh', 'eighth', 'ninth', 'tenth', 'eleventh', 'twelfth']
+    gifts = ['twelve Drummers Drumming, ', 'eleven Pipers Piping, ', 
+            'ten Lords-a-Leaping, ', 'nine Ladies Dancing, ', 
+            'eight Maids-a-Milking, ', 'seven Swans-a-Swimming, ', 
+            'six Geese-a-Laying, ', 'five Gold Rings, ', 
+            'four Calling Birds, ', 'three French Hens, ', 
+            'two Turtle Doves, and ', 'a Partridge in a Pear Tree.']
+    
+    # Initalizing the list to store the lyrics
+    recite = []
+
+    for i in range(start_verse - 1, end_verse):
+        verse = "On the {} day of Christmas my true love gave to me: ".format(days[i]) + "".join(gifts[11-i:])
+        recite.append(verse)
+
+    return recite
