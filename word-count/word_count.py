92f0fc2f2438d9557b1400f85a803fd1c14bd033
Rahul <32988067+rahulkumar1m@users.noreply.github.com>
2021-03-17 10:43:50 +0530

    Solution to word count problem

--- word-count/word_count.py
+++ word-count/word_count.py
@@ -1,2 +1,15 @@
+import re
+
 def count_words(sentence):
-    pass
+    # extract all the words as per definition
+    sentence = re.findall(r"\b[\w'-]+\b", sentence.lower().replace('_', ' '))
+    counts = {}
+
+    # Counting the frequency of each words
+    for word in sentence:
+        if word in counts:
+            counts[word] += 1
+        else:
+            counts[word] = 1
+    
+    return counts
