b4368c0eb0cc052f0faab58ab1f1210c37a9f71f
Rahul <32988067+rahulkumar1m@users.noreply.github.com>
2021-03-17 14:47:41 +0530

    Solution of sum-of-multiple

--- sum-of-multiples/sum_of_multiples.py
+++ sum-of-multiples/sum_of_multiples.py
@@ -1,2 +1,15 @@
-def sum_of_multiples(limit, multiples):
-    pass
+def sum_of_multiples(limit : int, multiples)-> int:
+    # Initializing a set a to store all the mutiples 
+    multiple_set = set()
+
+    # Remove non-positive numbers from mutiple list
+    multiples = [x for x in multiples if x > 0]
+
+    # calculating all the mutiples
+    for i in range(limit):
+        for j in multiples:
+            if i % j == 0:
+                multiple_set.add(i)
+                break
+
+    return sum(multiple_set)
