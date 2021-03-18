f466b721e3df5d6f15cfb517790d7c53163ea3a3
Rahul <32988067+rahulkumar1m@users.noreply.github.com>
2021-03-18 09:05:29 +0530

    Solution pythagorean triplet

--- pythagorean-triplet/pythagorean_triplet.py
+++ pythagorean-triplet/pythagorean_triplet.py
@@ -1,2 +1,2 @@
-def triplets_with_sum(number):
-    pass
+def triplets_with_sum(number: int):
+    return [[a, b, number - a - b] for a in range(number // 3) for b in range(a + 1, number // 2) if a**2 + b**2 == (number-a-b)**2]
