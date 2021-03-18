8dcef3a585e968f2fcc1d736753d20b89bd02ccb
Rahul <32988067+rahulkumar1m@users.noreply.github.com>
2021-03-18 08:41:14 +0530

    Solution to simple cipher

--- simple-cipher/simple_cipher.py
+++ simple-cipher/simple_cipher.py
@@ -1,9 +1,23 @@
+import random
+from string import ascii_lowercase as letters
+from itertools import cycle
+
 class Cipher:
     def __init__(self, key=None):
-        pass
+        if key is None:
+            self.key = "".join(random.choice(letters) for _ in range(100))
+        else:
+            self.key = key
 
-    def encode(self, text):
-        pass
+    def encode(self, text: str):
+        encoded = []
+        for ch1, ch2 in zip(text, cycle(self.key)):
+            encoded.append(letters[(ord(ch1) % 97 + ord(ch2) % 97) % 26])
+        return "".join(encoded)
 
-    def decode(self, text):
-        pass
+
+    def decode(self, text: str):
+        decoded = []
+        for ch1, ch2 in zip(text, cycle(self.key)):
+            decoded.append(letters[(ord(ch1) % 97 - ord(ch2) % 97) % 26])
+        return "".join(decoded)
