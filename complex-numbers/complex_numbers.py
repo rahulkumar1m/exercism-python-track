e1169301484e6b9e78e85686b0a98ebd3fee6acf
Rahul <32988067+rahulkumar1m@users.noreply.github.com>
2021-03-19 10:23:54 +0530

    Solution - Complex Numbers

--- complex-numbers/complex_numbers.py
+++ complex-numbers/complex_numbers.py
@@ -1,27 +1,33 @@
+import math
+
 class ComplexNumber:
     def __init__(self, real, imaginary):
-        pass
+        self.real = real
+        self.imaginary = imaginary
 
     def __eq__(self, other):
-        pass
+        return (self.real == other.real) and (self.imaginary == other.imaginary)
 
     def __add__(self, other):
-        pass
+        return ComplexNumber(self.real + other.real, self.imaginary + other.imaginary)
 
     def __mul__(self, other):
-        pass
+        return ComplexNumber(self.real * other.real - self.imaginary * other.imaginary, self.imaginary * other.real + self.real * other.imaginary)
 
     def __sub__(self, other):
-        pass
+        return ComplexNumber(self.real - other.real, self.imaginary - other.imaginary)
 
     def __truediv__(self, other):
-        pass
+        real_part = (self.real * other.real + self.imaginary * other.imaginary)/(other.real**2 + other.imaginary**2)
+        imaginary_part = (self.imaginary * other.real - self.real * other.imaginary)/(other.real**2 + other.imaginary**2)
+        return ComplexNumber(real_part, imaginary_part)
 
     def __abs__(self):
-        pass
+        return math.sqrt(self.real**2 + self.imaginary**2)
 
     def conjugate(self):
-        pass
+        return ComplexNumber(self.real, -1 * self.imaginary)
 
     def exp(self):
-        pass
+        e_a = math.exp(self.real)
+        return ComplexNumber(e_a*math.cos(self.imaginary), e_a*math.sin(self.imaginary))
