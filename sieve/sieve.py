9305eb7ae2dd5b84d166feccaf7c18c52e55456b
Rahul <32988067+rahulkumar1m@users.noreply.github.com>
2021-03-19 17:24:46 +0530

    Solution - Sieve of Eratosthenes problem

--- sieve/sieve.py
+++ sieve/sieve.py
@@ -1,2 +1,10 @@
-def primes(limit):
-    pass
+def primes(limit: int):
+    prime = [True for i in range(limit+1)]
+    p = 2
+    while(p**2 <= limit):
+        if prime[p] == True:
+            for i in range(p**2, limit + 1, p):
+                prime[i] = False
+        p += 1
+    
+    return [p for p in range(2, limit + 1) if prime[p] == True]
