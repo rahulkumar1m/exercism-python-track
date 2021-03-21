06288abff2bc1a8729d572d1b0b44cf07b3755e1
Rahul <32988067+rahulkumar1m@users.noreply.github.com>
2021-03-21 15:37:03 +0530

    Solution to tree building problem

--- tree-building/tree_building.py
+++ tree-building/tree_building.py
@@ -1,50 +1,40 @@
-class Record:
-    def __init__(self, record_id, parent_id):
-        self.record_id = record_id
-        self.parent_id = parent_id
+from typing import NamedTuple
+
+
+class Record(NamedTuple):
+    record_id: int
+    parent_id: int
+
+    def __lt__(self, other):
+        return self.record_id < other.record_id
 
 
 class Node:
     def __init__(self, node_id):
         self.node_id = node_id
         self.children = []
+        self.add_child = self.children.append
 
 
 def BuildTree(records):
-    root = None
-    records.sort(key=lambda x: x.record_id)
-    ordered_id = [i.record_id for i in records]
-    if records:
-        if ordered_id[-1] != len(ordered_id) - 1:
-            raise ValueError('Tree must be continuous')
-        if ordered_id[0] != 0:
-            raise ValueError('Tree must start with id 0')
+    if not records:
+        return None
+
     trees = []
-    parent = {}
-    for i in range(len(ordered_id)):
-        for j in records:
-            if ordered_id[i] == j.record_id:
-                if j.record_id == 0:
-                    if j.parent_id != 0:
-                        raise ValueError('Root node cannot have a parent')
-                if j.record_id < j.parent_id:
-                    raise ValueError('Parent id must be lower than child id')
-                if j.record_id == j.parent_id:
-                    if j.record_id != 0:
-                        raise ValueError('Tree is a cycle')
-                trees.append(Node(ordered_id[i]))
-    for i in range(len(ordered_id)):
-        for j in trees:
-            if i == j.node_id:
-                parent = j
-        for j in records:
-            if j.parent_id == i:
-                for k in trees:
-                    if k.node_id == 0:
-                        continue
-                    if j.record_id == k.node_id:
-                        child = k
-                        parent.children.append(child)
-    if len(trees) > 0:
-        root = trees[0]
-    return root
+    records.sort()
+    for ii, record in enumerate(records):
+        record_id, parent_id = record 
+        if ii != record_id:
+            raise ValueError('Tree must be continuous and start with id 0')
+        if record_id < parent_id:
+            raise ValueError('Parent id must be lower than child id')
+        if record_id == parent_id != 0:
+            raise ValueError('Tree is a cycle')
+
+        node = Node(record_id)
+        trees.append(node)
+        if record_id != 0:
+            parent = trees[parent_id]
+            parent.add_child(node)
+
+    return trees[0]
