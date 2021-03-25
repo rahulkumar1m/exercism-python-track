e83e8e69f0098342f54cce4dba14627acfe180de
Rahul <32988067+rahulkumar1m@users.noreply.github.com>
2021-03-25 20:04:09 +0530

    Optimized or uncluttered word-count problem

--- word-count/word_count.py
+++ word-count/word_count.py
@@ -1,15 +1,14 @@
 import re
+from collections import defaultdict
 
 def count_words(sentence):
     # extract all the words as per definition
     sentence = re.findall(r"\b[\w'-]+\b", sentence.lower().replace('_', ' '))
-    counts = {}
+    counts = defaultdict(lambda: 0)
 
     # Counting the frequency of each words
     for word in sentence:
-        if word in counts:
-            counts[word] += 1
-        else:
-            counts[word] = 1
+        counts[word] += 1
     
     return counts
+ 
