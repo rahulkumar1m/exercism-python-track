2845feb79db4679fe7a5505caf9ab7ea4b9b3be0
Rahul <32988067+rahulkumar1m@users.noreply.github.com>
2021-03-26 10:35:13 +0530

    Solution scrabble problem

--- scrabble-score/scrabble_score.py
+++ scrabble-score/scrabble_score.py
@@ -1,2 +1,12 @@
-def score(word):
-    pass
+points = {}
+for letters, point in {'AEIOULNRST' : 1,
+            'DG' : 2,
+            'BCMP' : 3,
+            'FHVWY' : 4,
+            'K' : 5,
+            'JX' : 8,
+            'QZ' : 10}.items():
+    points.update({letter: point for letter in letters})
+
+def score(word: str) -> int:
+   return sum([points[letter] for letter in word.upper()])
