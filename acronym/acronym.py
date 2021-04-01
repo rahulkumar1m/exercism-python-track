49b8d7d86b39f0499bce6b17733121ab1a3bc577
Rahul <32988067+rahulkumar1m@users.noreply.github.com>
2021-04-01 13:01:05 +0530

    Solution Acronym

--- acronym/acronym.py
+++ acronym/acronym.py
@@ -1,2 +1,7 @@
-def abbreviate(words):
-    pass
+import re
+def abbreviate(words: str)-> str:
+    # make a list of all the words in the string
+    word = re.findall(r"[a-zA-Z']+", words)
+
+    # Join & return the uppercase of first letter of each word
+    return "".join([x[0].upper() for x in word])
