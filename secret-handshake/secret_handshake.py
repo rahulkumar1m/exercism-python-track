9f4eb6dd2dbb57dc6af5ece5a885dfa093b938c4
Rahul <32988067+rahulkumar1m@users.noreply.github.com>
2021-04-02 19:38:09 +0530

    Solution Secret Handshake

--- secret-handshake/secret_handshake.py
+++ secret-handshake/secret_handshake.py
@@ -1,2 +1,31 @@
-def commands(number):
-    pass
+handshake = {
+    1 : "wink",
+    10 : "double blink",
+    100 : "close your eyes",
+    1000 : "jump"
+}
+
+def commands(number: int):
+    # converting the number to binary format
+    num_bin = "{0:b}".format(number)
+    
+    # declaring list to store the secret handshake
+    secret = []
+
+    # initializing counter to count the number of places we have iterated in binary
+    counter = 0
+
+    # looping over places of binary number and 
+    for c in reversed(num_bin):
+        if c != '0':
+            secret.append(handshake[int(c)*(10**counter)])
+        counter += 1
+        if counter >= 4:
+            break
+
+    # reversing the list if the
+    if len(num_bin) >= 5:
+        if num_bin[-5] == "1":
+            secret = secret[::-1]
+
+    return secret
