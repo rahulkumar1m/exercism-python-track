b3e38a3a77488df696445bc24f5aa56f4c856170
Rahul <32988067+rahulkumar1m@users.noreply.github.com>
2021-05-20 00:04:34 +0530

    Solution to kindergarten problem

--- kindergarten-garden/kindergarten_garden.py
+++ kindergarten-garden/kindergarten_garden.py
@@ -1,3 +1,23 @@
 class Garden:
-    def __init__(self, diagram, students):
-        pass
+    def __init__(self, diagram: str, students = ['Alice', 'Bob', 'Charlie', 'David', 'Eve', 'Fred', 'Ginny', 'Harriet', 'Ileana', 'Joseph', 'Kincaid', 'Larry']):
+        self.diagram = diagram.splitlines()
+        self.students = sorted(students)
+        self.plant = {'V': 'Violets',
+                    'R': 'Radishes',
+                    'C': 'Clover',
+                    'G': 'Grass'
+        }
+
+    def plants(self, student: str) -> list:
+        # Finding the index of the sought student
+        idx = (self.students.index(student) + 1) * 2
+
+        # Declaring a list to store the result
+        result = []
+
+        # Iterating over the two lines of sills on windows to get the plants planted by sought student
+        for line in self.diagram:
+            result.append(self.plant[line[idx - 2]])
+            result.append(self.plant[line[idx - 1]])
+
+        return result
