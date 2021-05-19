cc5bf33d25caf27d4f0d1808462ddce2143c0c7d
Rahul <32988067+rahulkumar1m@users.noreply.github.com>
2021-05-20 00:43:06 +0530

    Solutio to Largest-Series-product problem

--- largest-series-product/largest_series_product.py
+++ largest-series-product/largest_series_product.py
@@ -1,2 +1,14 @@
 def largest_product(series, size):
-    pass
+    if size == 0:
+        return 1
+    elif (size < 0) | (size > len(series)) | ((series.isnumeric()) == False):
+        raise ValueError("Error")
+    
+    products = []
+    for i in range(len(series)-size + 1):
+        product = 1
+        for j in range(i, i+size):
+            product *= int(series[j])
+        products.append(product)
+
+    return max(products)
